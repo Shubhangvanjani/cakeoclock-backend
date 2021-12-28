2e9597a705048a0cb7e0134334ee7d58e785eee0
Shubhangvanjani <shubhangvanjani@gmail.com>
2021-12-28 08:21:38 +0530

    ft. product details api

--- server.js
+++ server.js
@@ -9,6 +9,15 @@ app.get('/', (req, res) => {
   res.send('Server is ready')
 })
 
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find((x) => x._id === req.params.id )
+  if(product){
+    res.send(product)
+  }else{
+    res.status(404).send({message:'Product not found'})
+  }
+})
+
 app.get('/api/products', (req, res) => {
   res.send(products)
 })
